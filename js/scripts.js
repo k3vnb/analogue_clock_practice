97a104845f3e414add51e04ca9e2e61a2ed19424
Kevin Boyle <papershack@gmail.com>
2018-05-14 13:37:50 -0700

    add function and setInterval to have clock update in real time

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-
+function runTheClock(){
 var date = new Date();
 console.log(date);
 let hr = date.getHours();
@@ -18,3 +18,6 @@ let secPosition = sec*360/60;
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
 SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+var interval = setInterval(runTheClock, 1000);
