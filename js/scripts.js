e099adba88b4b5bf662cccdb815a5623dd4fe2ee
Kevin Boyle <papershack@gmail.com>
2018-05-14 13:22:10 -0700

    add js & css set up for hand movement

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,12 @@
-function my_Clock() {
-  this.cur_date = new Date();
-  this.hours = this.cur_date.getHours();
-  this.minutes = this.cur_date.getMinutes();
-  this.seconds = this.cur_date.getSeconds();
-}
 
-my_Clock.prototype.run = function(){
-  {
-    setInterval(this.update.bind(this), 1000)
-  };
-  my_Clock.prototype.update = function(){
-    this.updateTime(1);
-    console.log(this.hours + ":" + this.minutes + ":" + this.seconds);      
-    }
-  }
-}
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+
+let hrPosition = 20;
+let minPosition = 130;
+let secPosition = 267;
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
